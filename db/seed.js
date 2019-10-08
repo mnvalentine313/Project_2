5b8c0afe28ae44ee2d42cfdde143c93925cbf7ec
Melaina Valentine <emme313@Melainas-MacBook-Pro.local>
2019-10-07 19:31:22 -0700

    Still more CSS

--- db/seed.js
+++ db/seed.js
@@ -9,3 +9,5 @@ Business.remove({})
     console.log("There is seed data")
     process.exit();
 });
+
+// https://www.oshunswimschool.com/uploads/4/6/6/6/46667031/published/a25f88b931ad4604785676db8f873504-oshun-goddess-goddess-art.jpg?1527720182
